304b9a8be35bb6e08ee4f3e9cec68b1dd571d8c3
Firat Göktepe <firat@naxnordic.com>
2021-02-14 10:10:49 +0300

    Slider codes moved from main.js into index.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-/** Main Slider **/
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-    }
-}, 3000);
+
 
 /**  Responsive Header **/
 const navMenu = () => {
